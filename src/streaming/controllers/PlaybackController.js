5e1e9c51f2a2cbaae0a3930a33b850cd7da7661a
Jonty Usborne <jonty.usborne@bbc.co.uk>
2016-10-27 17:46:12 +0100

    SMPHTML-1088 If currentTime < DVRWindow.start, return start of window, not end of it. TODO: Why is + 0.001 needed?

--- src/streaming/controllers/PlaybackController.js
+++ src/streaming/controllers/PlaybackController.js
@@ -308,12 +308,13 @@ function PlaybackController() {
         var actualTime;
 
         if (!DVRWindow) return NaN;
-
-        if ((currentTime >= DVRWindow.start) && (currentTime <= DVRWindow.end)) {
+        if (currentTime > DVRWindow.end) {
+            actualTime = Math.max(DVRWindow.end - streamInfo.manifestInfo.minBufferTime * 2, DVRWindow.start + 0.001);
+        } else if (currentTime < DVRWindow.start) {
+            actualTime = DVRWindow.start + 0.001;
+        } else {
             return currentTime;
         }
-
-        actualTime = Math.max(DVRWindow.end - streamInfo.manifestInfo.minBufferTime * 2, DVRWindow.start);
 
         return actualTime;
     }
