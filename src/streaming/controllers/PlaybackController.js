c929910599d6684ee45939f633b9d0122077d0aa
Robert Bryer <robert.bryer@bbc.co.uk>
2020-03-19 17:18:49 +0000

    Revert "Prevent live stream from jumping to start due to wrongly reported current time"
    This reverts commit 47ea1fdb4b0e6fe99dc69977107032bef29796c1.

--- src/streaming/controllers/PlaybackController.js
+++ src/streaming/controllers/PlaybackController.js
@@ -400,15 +400,13 @@ function PlaybackController() {
 
         if (currentTime > DVRWindow.end) {
             actualTime = Math.max(DVRWindow.end - streamInfo.manifestInfo.minBufferTime * 2, DVRWindow.start);
-        } else if (currentTime + 0.250 < DVRWindow.start && DVRWindow.start - currentTime > DVRWindow.start - 315360000) {
+        } else if (currentTime + 0.250 < DVRWindow.start) {
             // Checking currentTime plus 250ms as the 'timeupdate' is fired with a frequency between 4Hz and 66Hz
             // https://developer.mozilla.org/en-US/docs/Web/Events/timeupdate
             // http://w3c.github.io/html/single-page.html#offsets-into-the-media-resource
-            // Checking also duration of the DVR makes sense. We detected temporary situations in which currentTime
-            // is bad reported by the browser which causes playback to jump to start (315360000 = 1 year)
             actualTime = DVRWindow.start;
         } else {
-            actualTime = currentTime;
+            return currentTime;
         }
 
         return actualTime;
