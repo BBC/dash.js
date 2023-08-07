2f8eedb62e6ecce694e97637dec8f7f971e66183
Eirik Jo Björnerstedt <eirik.bjornerstedt@bbc.co.uk>
2023-08-07 12:04:03 +0100

    docs: update default

--- src/core/Settings.js
+++ src/core/Settings.js
@@ -91,7 +91,7 @@ import {HTTPRequest} from '../streaming/vo/metrics/HTTPRequest';
  *                bufferTimeAtTopQualityLongForm: 60,
  *                initialBufferLevel: NaN,
  *                stableBufferTime: 12,
- *                hybridSwitchBufferTime: 12,
+ *                hybridSwitchBufferTime: NaN,
  *                longFormContentDurationThreshold: 600,
  *                stallThreshold: 0.5,
  *                useAppendWindow: true,
@@ -276,8 +276,8 @@ import {HTTPRequest} from '../streaming/vo/metrics/HTTPRequest';
  * Initial buffer level before playback starts
  * @property {number} [stableBufferTime=12]
  * The time that the internal buffer target will be set to post startup/seeks (NOT top quality).
- * @property {number} [hybridSwitchBufferTime=12]
- * The buffer time that the hybrid rule will switch between throughput and BOLA at.
+ * @property {number} [hybridSwitchBufferTime=NaN]
+ * The buffer time that the hybrid rule will switch between throughput and BOLA at. Defaults to the value of `stableBufferTime`.
  *
  * When the time is set higher than the default you will have to wait longer to see automatic bitrate switches but will have a larger buffer which will increase stability.
  * @property {number} [stallThreshold=0.3]
