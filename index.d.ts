e641d731c254ba49574db8c7c69490530f510f94
Eirik Jo Björnerstedt <eirik.bjornerstedt@bbc.co.uk>
2023-08-24 11:19:51 +0100

    doc: update type

--- index.d.ts
+++ index.d.ts
@@ -1249,6 +1249,8 @@ declare namespace dashjs {
 
         attachSource(urlOrManifest: string | object, startTime?: number | string): void;
 
+        refreshManifest(callback: (manifest: object | null, error: unknown) => void): void;
+
         isReady(): boolean;
 
         preload(): void;
@@ -2626,8 +2628,6 @@ declare namespace dashjs {
         getXHRWithCredentialsForType(type: string): object;
 
         getDefaultUtcTimingSource(): UTCTiming;
-
-        refreshManifest(callback: (manifest: object | null, error: any) => void): void;
 
         reset(): void;
     }
