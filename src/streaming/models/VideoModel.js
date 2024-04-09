1f593489d23ec526224c75d3527ceb6eeee16cc6
Eirik Jo Björnerstedt <eirik.bjornerstedt@bbc.co.uk>
2024-04-09 11:08:28 +0100

    feat: remove ready state check

--- src/streaming/models/VideoModel.js
+++ src/streaming/models/VideoModel.js
@@ -238,13 +238,13 @@ function VideoModel() {
     }
 
     function addStalledStream(type) {
-
         if (type === null || !element || element.seeking || stalledStreams.indexOf(type) !== -1) {
             return;
         }
 
         stalledStreams.push(type);
-        if (settings.get().streaming.buffer.emitSyntheticStallEvents && element && stalledStreams.length === 1 && element.readyState >= Constants.VIDEO_ELEMENT_READY_STATES.HAVE_FUTURE_DATA) {
+
+        if (settings.get().streaming.buffer.emitSyntheticStallEvents && element && stalledStreams.length === 1) {
             logger.debug(`emitting synthetic waiting event and halting playback with playback rate 0`);
             // Halt playback until nothing is stalled.
             const event = document.createEvent('Event');
